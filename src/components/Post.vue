d8b971e7bbdadcf441704a565c878710e0987e9b
behong <sijm@naver.com>
2023-06-25 22:26:11 +0900

    style 설정하기 vue에서

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -5,7 +5,7 @@
         <span class="profile-name">{{post.name}}</span>
       </div>
 
-      <div class="post-body"></div>
+      <div class="post-body" :style="{backgroundImage:`url(${post.postImage}`}"></div>
 
       <div class="post-content">
         <p>{{post.likes}} Likes</p>
